fb1cd9818231d1c7fd99abe29874ac95914b11e1
forsrc <forsrc@gmail.com>
2021-09-11 23:23:47 +0900

    log

--- jredmine-server/src/main/java/com/forsrc/jredmine/server/aop/LogAspect.java
+++ jredmine-server/src/main/java/com/forsrc/jredmine/server/aop/LogAspect.java
@@ -29,7 +29,7 @@ public class LogAspect {
 	public void before(JoinPoint joinPoint) {
 		UUID uuid = getUuid();
 		THREAD_LOCAL_UUID.set(uuid);
-		LOG.info("[START]-------------> {}; {}; {}; {}; {}", uuid, joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+		LOG.info("[START]\t[Controller] {}; {}; {}; {}; {}", uuid, joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
 	}
 	
 	@Around(value = "execution(* com.forsrc..*Controller.*(..))")
@@ -41,7 +41,7 @@ public class LogAspect {
 			rt = joinPoint.proceed();
 		} catch (Throwable e) {
 			Long time = System.currentTimeMillis() - start;
-			LOG.warn("[Around Exception]--> {}; Time: {}ms; Exception: {}, {};", uuid, time, e.getClass().getName(), e.getMessage());
+			LOG.warn("[Around]\t[Controller Exception] {}; Time: {}ms; Exception: {}, {};", uuid, time, e.getClass().getName(), e.getMessage());
 //			Map<String, Object> message = new HashMap<>(3);
 //			message.put("status", HttpStatus.INTERNAL_SERVER_ERROR.toString());
 //			message.put("message", e.getMessage());
@@ -52,7 +52,7 @@ public class LogAspect {
 			
 		}
 		Long time = System.currentTimeMillis() - start;
-		LOG.info("[Around]------------> {}; Time: {}ms; {}", uuid, time, rt);
+		LOG.info("[Around]\t[Controller] {}; Time: {}ms; {}", uuid, time, rt);
 		return rt;
 	}
 	
@@ -60,7 +60,7 @@ public class LogAspect {
 	public void after(JoinPoint joinPoint) {
 		UUID uuid = getUuid();
 		// LOG.info("[END]---------------> {}; {}; {}; {}; {}", uuid, joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget(), joinPoint);
-		LOG.info("[END]---------------> {};", uuid);
+		LOG.info("[END]\t[Controller] {};", uuid);
 
 	}
 
@@ -73,5 +73,38 @@ public class LogAspect {
 			}
 		}
 		return uuid;
+	}
+	
+	
+	@Before(value = "execution(* com.forsrc..*Service.*(..))")
+	public void beforeService(JoinPoint joinPoint) {
+		UUID uuid = getUuid();
+		THREAD_LOCAL_UUID.set(uuid);
+		LOG.info("[START]\t[Service] {}; {}; {}; {}; {}", uuid, joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+	}
+	
+	@Around(value = "execution(* com.forsrc..*Service.*(..))")
+	public Object aroundService(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+		UUID uuid = getUuid();
+		Object rt = null;
+		try {
+			rt = joinPoint.proceed();
+		} catch (Throwable e) {
+			Long time = System.currentTimeMillis() - start;
+			LOG.warn("[Around]\t[Service Exception] {}; Time: {}ms; Exception: {}, {};", uuid, time, e.getClass().getName(), e.getMessage());
+			throw e;
+			
+		}
+		Long time = System.currentTimeMillis() - start;
+		LOG.info("[Around]\t[Service] {}; Time: {}ms; {}", uuid, time, rt);
+		return rt;
+	}
+	
+	@After(value = "execution(* com.forsrc..*Service.*(..))")
+	public void afterService(JoinPoint joinPoint) {
+		UUID uuid = getUuid();
+		LOG.info("[END]\t[Service] {};", uuid);
+
 	}
 }
